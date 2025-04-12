9ad674f2a7f09fc06fba63f4b920a27bb3dc4f7b
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-04-12 15:30:41 +0530

    added buffer component

---
+++ src/Components/Buffer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Buffer = () => {
+  return (
+    <>
+      <div className="w-full  flex justify-center items-center" >
+        <img className="mx-auto"
+          src="https://i.giphy.com/PUYgk3wpNk0WA.webp"
+          alt=""/>
+      </div>
+    </>
+  );
+};
+
+export default Buffer;
